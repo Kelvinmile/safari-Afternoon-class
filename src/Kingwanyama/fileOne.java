682f6eda6b405e5e356de45785ccc50687a56a65
Kelvinmile <milekelvin13207@gmail.com>
2019-02-25 16:53:29 +0300

    first commit

---
+++ src/Kingwanyama/fileOne.java
@@ -0,0 +1,10 @@
+package Kingwanyama;
+
+public class fileOne {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("my name is miles");
+        System.out.println("i love coding");
+        System.out.println(24+79);
+    }
+}
---
+++ src/Kingwanyama/fileTwo.java
@@ -0,0 +1,7 @@
+package Kingwanyama;
+
+public class fileTwo {
+    public static void main(String[] args) {
+        System.out.println("i am "+20+"years old");
+    }
+}
